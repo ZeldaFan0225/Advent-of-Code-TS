9301eea362078f72c6e52a46373a5f0890682bba
ZeldaFan0225 <78901316+ZeldaFan0225@users.noreply.github.com>
2023-12-13 18:41:20 +0100

    simplify day 13

--- src/years/2023/day_13.ts
+++ src/years/2023/day_13.ts
@@ -2,8 +2,8 @@ export const INPUT_SPLIT = "\n\n";
 export function part_1(input: string[]): number {
     let scores = 0
     for(let block of input) {
-        let s = getVerticalScore(block.split("\n"))
-        if(s === null) s = (getHorizontalScore(block.split("\n")) ?? 0) * 100
+        let s = getVerticalScore(block.split("\n"), false)
+        if(s === null) s = (getHorizontalScore(block.split("\n"), false) ?? 0) * 100
 
         scores += s
     }
@@ -15,8 +15,8 @@ export function part_1(input: string[]): number {
 export function part_2(input: string): number {
     let scores = 0
     for(let block of input) {
-        let s = p2getVerticalScore(block.split("\n"))
-        if(s === null) s = (p2getHorizontalScore(block.split("\n")) ?? 0) * 100
+        let s = getVerticalScore(block.split("\n"), true)
+        if(s === null) s = (getHorizontalScore(block.split("\n"), true) ?? 0) * 100
 
         scores += s
     }
@@ -25,37 +25,7 @@ export function part_2(input: string): number {
 }
 
 
-
-function getHorizontalScore(lines: string[]): number | null {
-    const mirrored = lines.splice(0, 1)
-    let mirror_index: number | null = null;
-    while(lines.length) {
-        let i = 0
-        while(lines[i] === mirrored[i]) {
-            i++
-        }
-        if(i > 0 && (i === lines.length || i === mirrored.length)) {
-            mirror_index = mirrored.length;
-            break;
-        }
-        mirrored.unshift(lines.splice(0, 1)[0]!)
-    }
-
-    return mirror_index
-}
-
-
-function getVerticalScore(temp: string[]): number | null {
-    const lines: string[] = []
-    for(let i = 0; i < temp.length; i++) {
-        for(let j = 0; j < temp[0]!.length; j++) {
-            lines[j] = (lines[j] ?? "") + temp[i]![j]!
-        }
-    }
-    return getHorizontalScore(lines)
-}
-
-function p2getHorizontalScore(lines: string[]): number | null {
+function getHorizontalScore(lines: string[], part2 = false): number | null {
     const mirrored = lines.splice(0, 1)
     let mirror_index: number | null = null;
     while(lines.length) {
@@ -63,7 +33,7 @@ function p2getHorizontalScore(lines: string[]): number | null {
         let smudge_used = false;
         function compareLines(l: string, m: string) {
             if(!l || !m) return false;
-            if(smudge_used) return l === m;
+            if(smudge_used || !part2) return l === m;
             let tochange = 0
             for(let ind = 0; ind < l.length; ind++) {
                 if(l[ind] !== m[ind]) tochange++;
@@ -78,7 +48,7 @@ function p2getHorizontalScore(lines: string[]): number | null {
             i++
         }
         if(i > 0 && (i === lines.length || i === mirrored.length)) {
-            if(smudge_used) {
+            if(smudge_used || !part2) {
                 mirror_index = mirrored.length;
                 break;
             }
@@ -90,12 +60,12 @@ function p2getHorizontalScore(lines: string[]): number | null {
 }
 
 
-function p2getVerticalScore(temp: string[]): number | null {
+function getVerticalScore(temp: string[], part2 = false): number | null {
     const lines: string[] = []
     for(let i = 0; i < temp.length; i++) {
         for(let j = 0; j < temp[0]!.length; j++) {
             lines[j] = (lines[j] ?? "") + temp[i]![j]!
         }
     }
-    return p2getHorizontalScore(lines)
+    return getHorizontalScore(lines, part2)
 }
