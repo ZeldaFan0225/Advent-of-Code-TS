c20dc144c1d5ef2f34c8b678d5405d8aaecf8e33
ZeldaFan0225 <78901316+ZeldaFan0225@users.noreply.github.com>
2023-12-23 22:45:35 +0100

    day 23 complete :)

--- src/years/2023/day_23.ts
+++ src/years/2023/day_23.ts
@@ -1,19 +1,31 @@
+/*
+=========================
+Interfaces
+=========================
+ */
+
 interface Coordinate {
     x: number,
     y: number
 }
 
 interface Tile extends Coordinate {
-    c: string
+    c: string,
+    visited?: boolean,
+    from_node?: number,
+    distance?: number
 }
 
 interface QueueCoordinate extends Coordinate {
-    path: string
+    visited: Set<string>
 }
 
-interface DiscoveredCoordinate extends Coordinate {
-    distance: number
+interface Node extends Coordinate {
+    id: number,
+    connected_to: Set<number>
 }
+
+
 
 export const INPUT_SPLIT = "\n";
 export function part_1(input: string[]): number {
@@ -24,9 +36,138 @@ export function part_1(input: string[]): number {
 
 export function part_2(input: string[]): number {
     const {map, start, end} = parseMap(input, 2)
-    // 4887 is too low
-    return findLongestPath(map, start, end)
+    const {nodes, distanceMap} = getNodeRepresentation(map, start, end)
+    return getPathLengthsFromNodes(nodes, 0, new Set(), distanceMap)
 }
+
+
+
+/*
+=========================
+Part 1 specific functions
+=========================
+ */
+
+
+function findLongestPath(map: Tile[][], start: Coordinate, end: Coordinate) {
+    let queue: QueueCoordinate[] = [{...start, visited: new Set()}]
+    const distances = new Map<string, number>
+    while(queue.length) {
+        const current = queue.shift()!
+        const currentTile = map[current.y]![current.x]!
+        if(!currentTile) continue;
+
+        current.visited.add(`${current.x},${current.y}`)
+
+        if(current.x === end.x && current.y === end.y) {
+            // minus 1 because we don't count the start tile
+            distances.set([...current.visited].join(" "), current.visited.size - 1)
+            continue;
+        }
+
+        for(let neighbor of getNeighbors(map, current, current.visited)) {
+            queue.unshift({x: neighbor.x, y: neighbor.y, visited: new Set(current.visited)})
+        }
+    }
+
+    return Math.max(...distances.values())
+}
+
+
+
+/*
+=========================
+Part 2 specific functions
+=========================
+ */
+
+
+function getConnectionHash(node1: number, node2: number) {
+    return `${Math.min(node1, node2)}-${Math.max(node1, node2)}`
+}
+
+
+function getNodeRepresentation(map: Tile[][], start: Coordinate) {
+    const queue = [{...start, coming_from: 0, distance: -1}]
+    const nodes: Node[] = []
+    const distanceMap = new Map<string, number>()
+    while(queue.length) {
+        const process = queue.shift()!
+        if(map[process.y]?.[process.x]?.visited) {
+            if(map[process.y]?.[process.x]?.from_node !== process.coming_from) {
+                // when current node has been discovered but from a different node
+                const node1 = nodes.find(n => n.id === process.coming_from)!
+                const node2 = nodes.find(n => n.id === map[process.y]![process.x]!.from_node!)!
+                distanceMap.set(getConnectionHash(node1.id, node2.id), map[process.y]?.[process.x]?.distance! + process.distance + 1)
+                node1.connected_to.add(node2.id)
+                node2.connected_to.add(node1.id)
+            }
+            continue
+        }
+        map[process.y]![process.x]!.visited = true
+        const neighbors = getNeighbors(map, process)
+        let coming_from = process.coming_from
+        let distance = process.distance + 1
+        if(neighbors.length > 2 || neighbors.length === 1) {
+            // new node found
+            const prev_node = nodes.find(n => n.id === process.coming_from)
+            prev_node?.connected_to.add(nodes.length)
+            nodes.push({id: nodes.length, connected_to: new Set([process.coming_from]), x: process.x, y: process.y})
+            distanceMap.set(`${Math.min(process.coming_from, nodes.length - 1)}-${Math.max(process.coming_from, nodes.length - 1)}`, distance)
+            coming_from = nodes.length - 1
+            distance = 0
+        }
+        map[process.y]![process.x]!.from_node = coming_from
+        map[process.y]![process.x]!.distance = distance
+        for(let neighbor of neighbors) {
+            queue.push({
+                x: neighbor.x,
+                y: neighbor.y,
+                coming_from,
+                distance
+            })
+        }
+    }
+
+    nodes[0]?.connected_to.delete(0)
+    distanceMap.delete("0-0")
+
+    return {nodes, distanceMap}
+}
+
+
+function getPathLengthsFromNodes(nodes: Node[], node_i: number, visited: Set<number>, distanceMap: Map<string, number>): number {
+    visited.add(node_i)
+    if(node_i === nodes.length - 1) return getTotalDistanceFromNodes([...visited], distanceMap)
+
+    const distances: number[] = []
+
+    const neighbors = nodes[node_i]!.connected_to
+    for(let neighbor of neighbors) {
+        if(visited.has(neighbor)) continue;
+        distances.push(getPathLengthsFromNodes(nodes, neighbor, new Set(visited), distanceMap))
+    }
+
+    return Math.max(...distances)
+}
+
+
+function getTotalDistanceFromNodes(nodes: number[], distanceMap: Map<string, number>) {
+    let total = 0
+    for(let i = 1; i < nodes.length; i++) {
+        total += distanceMap.get(getConnectionHash(nodes[i - 1]!, nodes[i]!))!
+    }
+    return total
+}
+
+
+
+/*
+=========================
+Utility functions
+=========================
+ */
+
 
 function parseMap(input: string[], part: 1 | 2) {
     const map: Tile[][] = []
@@ -50,73 +191,29 @@ function parseMap(input: string[], part: 1 | 2) {
     return {map, start, end}
 }
 
-function findLongestPath(map: Tile[][], start: Coordinate, end: Coordinate) {
-    let queue: QueueCoordinate[] = [{...start, path: " "}]
-    const distances = new Map<string, number>
-    while(queue.length) {
-        const current = queue.shift()!
-        const currentTile = map[current.y]![current.x]!
-        if(!currentTile) continue;
 
-        const new_path = current.path + `${current.x},${current.y} `
-
-        // filter out any tiles that went through the current tile already as they are guaranteed to have a shorter path; they can be revisited using the longest path through this tile
-        queue = queue.filter(q => !q.path.includes(` ${current.x},${current.y} `))
-
-        if(current.x === end.x && current.y === end.y) {
-            // minus 1 because we don't count the start tile
-            distances.set(new_path, new_path.trim().split(" ").length - 1)
-            continue;
-        }
-
-        for(let neighbor of getNeighbors(map, current, new_path)) {
-            queue.push({x: neighbor.x, y: neighbor.y, path: new_path})
-        }
-        //console.log(queue)
-    }
-
-    /*for(let path of distances.keys()) {
-        printPath(map, path)
-    }*/
-
-    console.log(distances.values())
-    return Math.max(...distances.values())
-}
-
-function getNeighbors(map: Tile[][], coordinate: Coordinate, path: string): Tile[] {
+function getNeighbors(map: Tile[][], coordinate: Coordinate, path?: Set<string>): Tile[] {
     const {x, y} = coordinate
     const result: Tile[] = []
     if(
         map[y]?.[x - 1] &&
         [".", "<"].includes(map[y]?.[x - 1]?.c ?? "") &&
-        !path.includes(` ${x - 1},${y} `)
+        !path?.has(`${x - 1},${y}`)
     ) result.push(map[y]![x - 1]!)
     if(
         map[y]?.[x + 1] &&
         [".", ">"].includes(map[y]?.[x + 1]?.c ?? "") &&
-        !path.includes(` ${x + 1},${y} `)
+        !path?.has(`${x + 1},${y}`)
     ) result.push(map[y]![x + 1]!)
     if(
         map[y - 1]?.[x] &&
         [".", "^"].includes(map[y - 1]?.[x]?.c ?? "") &&
-        !path.includes(` ${x},${y - 1} `)
+        !path?.has(`${x},${y - 1}`)
     ) result.push(map[y - 1]![x]!)
     if(
         map[y + 1]?.[x] &&
         [".", "v"].includes(map[y + 1]?.[x]?.c ?? "") &&
-        !path.includes(` ${x},${y + 1} `)
+        !path?.has(`${x},${y + 1}`)
     ) result.push(map[y + 1]![x]!)
     return result
-}
-
-function printPath(map: Tile[][], path: string) {
-    for(let y = 0; y < map.length; y++) {
-        let line = ""
-        for(let x = 0; x < map[y]!.length; x++) {
-            if(path.includes(` ${x},${y} `)) line += "O"
-            else line += "."//map[y]![x]!.c
-        }
-        console.log(line)
-    }
-    console.log("\n\n")
 }
