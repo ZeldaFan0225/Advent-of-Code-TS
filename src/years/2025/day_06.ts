ce86f98de3efa8827857c9e7a5e2d86faa56ae7a
ZeldaFan0225 <78901316+ZeldaFan0225@users.noreply.github.com>
2025-12-06 09:23:01 +0100

    back to number

--- src/years/2025/day_06.ts
+++ src/years/2025/day_06.ts
@@ -2,14 +2,14 @@ export const INPUT_SPLIT = "\n";
 export function part_1(input: [string, string, string, string]): string {
     const nums = input;
     const o = nums.splice(-1, 1)[0]!
-    const rows = nums.map(l => l.split(" ").filter(Boolean).map(BigInt)) as bigint[][]
+    const rows = nums.map(l => l.split(" ").filter(Boolean).map(Number)) as number[][]
     const operations = o.split(" ").filter(Boolean).map(c => c.charCodeAt(0) - 42) // 0 = *, 1 = +
-    let sum = 0n
+    let sum = 0
     for(let i = 0; i < operations.length; i++) {
         if(operations[i] === 0) {
-            sum += rows.map(r => r[i]!).reduce((a, b) => a * b, 1n)
+            sum += rows.map(r => r[i]!).reduce((a, b) => a * b, 1)
         } else {
-            sum += rows.map(r => r[i]!).reduce((a, b) => a + b, 0n)
+            sum += rows.map(r => r[i]!).reduce((a, b) => a + b, 0)
         }
     }
     return `${sum}`
