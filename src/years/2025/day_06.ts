67af4ac3b745274cf30f5203766886e71b120b53
ZeldaFan0225 <78901316+ZeldaFan0225@users.noreply.github.com>
2025-12-06 09:22:28 +0100

    day 6-1

---
+++ src/years/2025/day_06.ts
@@ -0,0 +1,21 @@
+export const INPUT_SPLIT = "\n";
+export function part_1(input: [string, string, string, string]): string {
+    const nums = input;
+    const o = nums.splice(-1, 1)[0]!
+    const rows = nums.map(l => l.split(" ").filter(Boolean).map(BigInt)) as bigint[][]
+    const operations = o.split(" ").filter(Boolean).map(c => c.charCodeAt(0) - 42) // 0 = *, 1 = +
+    let sum = 0n
+    for(let i = 0; i < operations.length; i++) {
+        if(operations[i] === 0) {
+            sum += rows.map(r => r[i]!).reduce((a, b) => a * b, 1n)
+        } else {
+            sum += rows.map(r => r[i]!).reduce((a, b) => a + b, 0n)
+        }
+    }
+    return `${sum}`
+}
+
+
+export function part_2(input: string): number {
+    return input.length
+}
